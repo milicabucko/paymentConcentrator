4f8c9f9ee63bb44e629540de9b2b08ff2d1fcd16
Dragisa <milica.milica.b@gmail.com>
2018-12-03 20:59:51 +0100

    Inicijalni commit

---
+++ src/main/java/com/sep/PaymentConcentrator/PaymentConcentratorApplication.java
@@ -0,0 +1,12 @@
+package com.sep.PaymentConcentrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentConcentratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentConcentratorApplication.class, args);
+	}
+}
